2a6f576823df67e8f70082f5f5467719ff636a1c
Abinaya <abipst@gmail.com>
2024-08-17 17:25:43 -0400

    adding feature file, step definition class for Dietician POST

--- src/test/java/com/api/EnvVariables/EnvVariables.java
+++ src/test/java/com/api/EnvVariables/EnvVariables.java
@@ -2,5 +2,8 @@ package com.api.EnvVariables;
 
 public class EnvVariables {
 	public static String token;
+	public static String dietician1_loginPassword;
+	public static String dietician1_Email;
+	public static String dietician1_ID;
 
 }
