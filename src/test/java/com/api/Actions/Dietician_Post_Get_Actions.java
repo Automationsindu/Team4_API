7525b6cb293a14a2b33b3d75558b76aaef6643e7
Abinaya <abipst@gmail.com>
2024-08-20 00:17:09 -0400

    adding more scenarios and code

--- src/test/java/com/api/Actions/Dietician_Post_Get_Actions.java
+++ src/test/java/com/api/Actions/Dietician_Post_Get_Actions.java
@@ -1,15 +1,20 @@
 package com.api.Actions;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import com.api.EnvVariables.EnvConstants;
 import com.api.EnvVariables.EnvVariables;
 import com.api.ReusableUtils.Reusable_CRUD_Operations;
+import com.api.ReusableUtils.UserExcelReader;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -17,76 +22,231 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Dietician_Post_Get_Actions {
-	
-	Reusable_CRUD_Operations restUtil= new Reusable_CRUD_Operations();
+
+	Reusable_CRUD_Operations restUtil = new Reusable_CRUD_Operations();
 	private String requestBody = "";
+	Response response;
 	String token;
-	
-	/*=================================Building request specification======================================*/
-	
-	public RequestSpecification buildRequest() throws FileNotFoundException
-	{
+	String ContactNumber;
+	String DateOfBirth;
+	String Education;
+	String Email;
+	String Firstname;
+	String HospitalCity;
+	String HospitalName;
+	String HospitalPincode;
+	String HospitalStreet;
+	String Lastname;
+
+	/*
+	 * =================================Building request
+	 * specification======================================
+	 */
+
+	public RequestSpecification buildRequest() throws FileNotFoundException {
 		RequestSpecification reqSpec;
 		reqSpec = restUtil.getRequestSpec();
 		return reqSpec;
 	}
-	 	
 
-	/*============================post request to create a dietician===============================================*/
+	public static String createJsonPayload(String... keyValuePairs) {
+		JsonObject json = new JsonObject();
+		for (int i = 0; i < keyValuePairs.length; i += 2) {
+			json.addProperty(keyValuePairs[i], keyValuePairs[i + 1]);
+		}
+		Gson gson = new Gson();
+		return gson.toJson(json);
+	}
 
-	public Response createDietician(RequestSpecification reqSpec) throws FileNotFoundException {
+	/*
+	 * ============================post request to create a
+	 * dietician===============================================
+	 */
+	public String buildValidRequestData(RequestSpecification reqSpec)
+			throws InvalidFormatException, IOException {
 		
-		String ContactNumber = RandomStringUtils.randomNumeric(10);
-		String DateOfBirth= (LocalDate.now().minus(Period.ofDays((new Random().nextInt(365 * 70))))).toString();
-		String Education = RandomStringUtils.randomAlphabetic(10);
-		String Email = RandomStringUtils.randomAlphanumeric(6) + "@ninja.com";
-		String Firstname = RandomStringUtils.randomAlphabetic(10);
-		String HospitalCity = RandomStringUtils.randomAlphabetic(10);
-		String HospitalName = RandomStringUtils.randomAlphabetic(10);
-		String HospitalPincode = RandomStringUtils.randomNumeric(6);
-		String HospitalStreet = RandomStringUtils.randomAlphabetic(10);
-		String Lastname = RandomStringUtils.randomAlphabetic(10);
+		ContactNumber = RandomStringUtils.randomNumeric(10);
+		DateOfBirth = (LocalDate.now().minus(Period.ofDays((new Random().nextInt(365 * 70))))).toString();
+		Education = RandomStringUtils.randomAlphabetic(10);
+		Email = RandomStringUtils.randomAlphanumeric(6) + "@ninja.com";
+		Firstname = RandomStringUtils.randomAlphabetic(10);
+		HospitalCity = RandomStringUtils.randomAlphabetic(10);
+		HospitalName = RandomStringUtils.randomAlphabetic(10);
+		HospitalPincode = RandomStringUtils.randomNumeric(6);
+		HospitalStreet = RandomStringUtils.randomAlphabetic(10);
+		Lastname = RandomStringUtils.randomAlphabetic(10);
 		
-		 // Construct JSON payload using Gson
-        JsonObject json = new JsonObject();
-        json.addProperty("ContactNumber", ContactNumber);
-        json.addProperty("DateOfBirth", DateOfBirth);
-        json.addProperty("Education", Education);
-        json.addProperty("Email", Email);
-        json.addProperty("Firstname", Firstname);
-        json.addProperty("HospitalCity", HospitalCity);
-        json.addProperty("HospitalName", HospitalName);
-        json.addProperty("HospitalPincode", HospitalPincode);
-        json.addProperty("HospitalStreet", HospitalStreet);
-        json.addProperty("Lastname", Lastname);
+		requestBody = createJsonPayload("ContactNumber", ContactNumber, "DateOfBirth", DateOfBirth, "Education",
+				Education, "Email", Email, "Firstname", Firstname, "HospitalCity", HospitalCity, "HospitalName",
+				HospitalName, "HospitalPincode", HospitalPincode, "HospitalStreet", HospitalStreet, "Lastname",
+				Lastname);
+		
+		System.out.println("Login request Body positive is : " + requestBody);
+		
+		return requestBody;
 
-        Gson gson = new Gson();
-        requestBody = gson.toJson(json);
+	}
+	
+	public String buildInValidRequestData(RequestSpecification reqSpec , String currentTag)
+			throws InvalidFormatException, IOException {
 		
-		System.out.println("Login request Body is : "+requestBody);
-		Response response = restUtil.create(reqSpec, EnvVariables.token, requestBody, EnvConstants.createDietician_Endpoint);
+		String trimmedCurrentTag = currentTag.startsWith("@") ? currentTag.substring(1) : currentTag;
+		System.out.println(trimmedCurrentTag);
 		
+		List<Map<String, String>> getUserData = (UserExcelReader.getData(EnvConstants.Excelpath,
+				"Dietician_Create"));
+		Map<String, String> rowdata = getUserData.stream()
+				.filter(row -> row.get("scenario").equals(trimmedCurrentTag)).findFirst()
+				.orElseThrow(() -> new RuntimeException("No matching data found for tag: " + trimmedCurrentTag));
+		ContactNumber = rowdata.get("ContactNumber");
+		DateOfBirth = rowdata.get("DateOfBirth");
+		Education = rowdata.get("Education");
+		Email = rowdata.get("Email");
+		Firstname = rowdata.get("Firstname");
+		HospitalCity = rowdata.get("HospitalCity");
+		HospitalName = rowdata.get("HospitalName");
+		HospitalPincode = rowdata.get("HospitalPincode");
+		HospitalStreet = rowdata.get("HospitalStreet");
+		Lastname = rowdata.get("Lastname");
+		
+		requestBody = createJsonPayload("ContactNumber", ContactNumber, "DateOfBirth", DateOfBirth, "Education",
+				Education, "Email", Email, "Firstname", Firstname, "HospitalCity", HospitalCity, "HospitalName",
+				HospitalName, "HospitalPincode", HospitalPincode, "HospitalStreet", HospitalStreet, "Lastname",
+				Lastname);
+		
+		System.out.println("Login request Body invalid : " + requestBody);
+		
+		return requestBody;
+
+	}
+	
+	public Response createDietician(RequestSpecification reqSpec, String requestData, String currentTag)
+			throws InvalidFormatException, IOException {
+
+		/*
+		 * String trimmedCurrentTag = currentTag.startsWith("@") ?
+		 * currentTag.substring(1) : currentTag; System.out.println(trimmedCurrentTag);
+		 * 
+		 * 
+		 * 
+		 * if (trimmedCurrentTag.equalsIgnoreCase("create_dietician_positive")) {
+		 * 
+		 * ContactNumber = RandomStringUtils.randomNumeric(10); DateOfBirth =
+		 * (LocalDate.now().minus(Period.ofDays((new Random().nextInt(365 *
+		 * 70))))).toString(); Education = RandomStringUtils.randomAlphabetic(10); Email
+		 * = RandomStringUtils.randomAlphanumeric(6) + "@ninja.com"; Firstname =
+		 * RandomStringUtils.randomAlphabetic(10); HospitalCity =
+		 * RandomStringUtils.randomAlphabetic(10); HospitalName =
+		 * RandomStringUtils.randomAlphabetic(10); HospitalPincode =
+		 * RandomStringUtils.randomNumeric(6); HospitalStreet =
+		 * RandomStringUtils.randomAlphabetic(10); Lastname =
+		 * RandomStringUtils.randomAlphabetic(10);
+		 * 
+		 * } else { List<Map<String, String>> getUserData =
+		 * (UserExcelReader.getData(EnvConstants.Excelpath, "Dietician_Create"));
+		 * Map<String, String> rowdata = getUserData.stream() .filter(row ->
+		 * row.get("scenario").equals(trimmedCurrentTag)).findFirst() .orElseThrow(() ->
+		 * new RuntimeException("No matching data found for tag: " +
+		 * trimmedCurrentTag)); ContactNumber = rowdata.get("ContactNumber");
+		 * DateOfBirth = rowdata.get("DateOfBirth"); Education =
+		 * rowdata.get("Education"); Email = rowdata.get("Email"); Firstname =
+		 * rowdata.get("Firstname"); HospitalCity = rowdata.get("HospitalCity");
+		 * HospitalName = rowdata.get("HospitalName"); HospitalPincode =
+		 * rowdata.get("HospitalPincode"); HospitalStreet =
+		 * rowdata.get("HospitalStreet"); Lastname = rowdata.get("Lastname"); }
+		 * 
+		 * requestBody = createJsonPayload("ContactNumber", ContactNumber,
+		 * "DateOfBirth", DateOfBirth, "Education", Education, "Email", Email,
+		 * "Firstname", Firstname, "HospitalCity", HospitalCity, "HospitalName",
+		 * HospitalName, "HospitalPincode", HospitalPincode, "HospitalStreet",
+		 * HospitalStreet, "Lastname", Lastname);
+		 * 
+		 * 
+		 * System.out.println("Login request Body is : " + requestBody);
+		 */
+		
+		String trimmedCurrentTag = currentTag.startsWith("@")? currentTag.substring(1) : currentTag ;
+		
+		System.out.println("trimmedCurrentTag is : " + trimmedCurrentTag);
+		 
+		    // sending request
+		    switch(trimmedCurrentTag) {
+		    case "create_dietician_positive":
+		    	response = restUtil.create(reqSpec, EnvVariables.token, requestBody,
+						EnvConstants.createDietician_Endpoint);
+		    	break;
+		    
+		    case "create_dietician_invalidData":
+		    	response = restUtil.create(reqSpec, EnvVariables.token, requestBody,
+						EnvConstants.createDietician_Endpoint);
+		    	break;
+		    case "create_dietician_invalidMethod":
+		    	response = restUtil.put(reqSpec, EnvVariables.token, requestBody,
+						EnvConstants.createDietician_Endpoint);
+		    	
+		    case "create_dietician_invalidEndpoint":
+		    	response = restUtil.create(reqSpec, EnvVariables.token, requestBody,
+						EnvConstants.Invalid_createDietician_Endpoint);
+		    	break;
+		    case "create_dietician_invalidContentType":
+		    	//RequestSpecification defaultSpec = restUtil.getRequestSpec();
+		    	RequestSpecification textspec = reqSpec.contentType(io.restassured.http.ContentType.TEXT);
+		    	response = restUtil.create(textspec, EnvVariables.token, requestBody,
+						EnvConstants.createDietician_Endpoint);
+		    	break;
+		    	default :
+		    		throw new RuntimeException("no matching tag :" +trimmedCurrentTag);
+		    }
+		 
+		 
+		 
+		 
+		//Response response = restUtil.create(reqSpec, EnvVariables.token, requestBody,
+				//EnvConstants.createDietician_Endpoint);
+
 		return response;
 	}
 
+	/*
+	 * ===========================Storing the required response data in Environment
+	 * variables==============================================
+	 */
 
-	/*===========================Storing the required response data in Env variables==============================================*/
+	public void storeDieticianInfo(Response response, String dieticianCount) {
+		// System.out.println("response sending from actions
+		// "+response.asPrettyString());
 
-	public void storeDieticianInfo(Response response)
-	{
-		//System.out.println("response sending from actions "+response.asPrettyString());
-		String id= restUtil.extractStringFromResponse(response, "id");
-		String loginPassword= restUtil.extractStringFromResponse(response, "loginPassword");
-		String email= restUtil.extractStringFromResponse(response, "Email");
-		System.out.println("The token from the response is "+ id);
-		System.out.println("The token from the response is "+ loginPassword);
-		System.out.println("The token from the response is "+ email);
-		EnvVariables.dietician1_ID= id;
-		EnvVariables.dietician1_Email = email;
-		EnvVariables.dietician1_loginPassword = loginPassword;
-		System.out.println("The token stored in EnvVariables.token is "+ EnvVariables.dietician1_ID);
-		System.out.println("The token stored in EnvVariables.dietician1_Email is "+ EnvVariables.dietician1_Email);
-		System.out.println("The token stored in EnvVariables.dietician1_loginPassword is "+ EnvVariables.dietician1_loginPassword);
+		if (response.getStatusCode() == 201) {
+			if (dieticianCount.equalsIgnoreCase("Dietician1")) {
+				String id = restUtil.extractStringFromResponse(response, "id");
+				String loginPassword = restUtil.extractStringFromResponse(response, "loginPassword");
+				String email = restUtil.extractStringFromResponse(response, "Email");
+				EnvVariables.dietician1_ID = id;
+				EnvVariables.dietician1_Email = email;
+				EnvVariables.dietician1_loginPassword = loginPassword;
+				System.out.println("The token stored in EnvVariables.dietician1_ID is " + EnvVariables.dietician1_ID);
+				System.out.println(
+						"The token stored in EnvVariables.dietician1_Email is " + EnvVariables.dietician1_Email);
+				System.out.println("The token stored in EnvVariables.dietician1_loginPassword is "
+						+ EnvVariables.dietician1_loginPassword);
+
+			} else if (dieticianCount.equalsIgnoreCase("Dietician2")) {
+				String id = restUtil.extractStringFromResponse(response, "id");
+				String loginPassword = restUtil.extractStringFromResponse(response, "loginPassword");
+				String email = restUtil.extractStringFromResponse(response, "Email");
+				EnvVariables.dietician2_ID = id;
+				EnvVariables.dietician2_Email = email;
+				EnvVariables.dietician2_loginPassword = loginPassword;
+				System.out.println("The token stored in EnvVariables.dietician2_ID is " + EnvVariables.dietician2_ID);
+				System.out.println(
+						"The token stored in EnvVariables.dietician2_Email is " + EnvVariables.dietician2_Email);
+				System.out.println("The token stored in EnvVariables.dietician2_loginPassword is "
+						+ EnvVariables.dietician2_loginPassword);
+
+			}
+
+		}
+
 	}
 
 }
--- src/test/resources/Feature/02.dietician_Post_Get.feature
+++ src/test/resources/Feature/02.dietician_Post_Get.feature
@@ -1,14 +1,41 @@
 #Author: Abinaya
+# only admin token
 
-@tag
 Feature: Validate Dietician Endpoints (POST, GET, GET All)
 
-	@tag1
-  Scenario: Check if admin is able to create dietician with valid data and token 
-   Given Set admin token and Admin creates Dietician POST request with valid data.
-    When Admin sends Dietician POST http request with endpoint
+  
+  @create_dietician_positive
+  Scenario Outline: Check if admin is able to create dietician with valid data and token 
+    Given Set admin token and Admin creates Dietician POST request with valid data
+    When Admin sends Dietician "<Dietician>" POST http request with endpoint
     Then Admin should receive status code 201 created and with response body having auto created dietician ID and login password
+
+    Examples: 
+      | Dietician	  | 
+      | Dietician1 	| 
+      | Dietician2 	|  
+      
+ @create_dietician_invalidData
+  Scenario: Check if admin is able to create dietician with invalid data
+    Given Set admin token and Admin creates Dietician POST request with invalid data
+    When Admin sends Dietician POST http request with endpoint
+    Then Admin should receive 400 Bad request
     
-
 	
-
+  @create_dietician_invalidMethod
+  Scenario: Check admin able to create dietician with valid data and invalid method
+    Given Set admin token and Admin creates Dietician POST request with valid data
+    When Admin sends Dietician PUT http request with endpoint
+    Then Admin should receive 405 method not allowed
+    
+  @create_dietician_invalidEndpoint
+  Scenario: Check admin able to create dietician with valid data and invalid endpoint
+    Given Set admin token and Admin creates Dietician POST request with valid data
+    When Admin sends Dietician POST http request with invalid endpoint
+    Then Admin should receive 404 not found
+    
+  @create_dietician_invalidContentType
+  Scenario: Check admin able to create dietician with valid data and invalid content type
+    Given Set admin token and Admin creates Dietician POST request with valid data 
+    When Admin send POST http request with valid endpoint and invalid content type
+    Then Admin should receive 415 unsupported media type
