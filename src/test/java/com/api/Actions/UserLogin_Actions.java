989b4b425a89d356968d7e332c309c3dc8db5f67
Automationsindu <sindooja216@gmail.com>
2024-08-22 07:47:37 -0500

    0822:Committing the latest changes to sindu's branch

--- src/test/java/com/api/Actions/UserLogin_Actions.java
+++ src/test/java/com/api/Actions/UserLogin_Actions.java
@@ -1,14 +1,17 @@
 package com.api.Actions;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.util.List;
+import java.util.Map;
+
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import com.api.EnvVariables.EnvConstants;
 import com.api.EnvVariables.EnvVariables;
 import com.api.ReusableUtils.Reusable_CRUD_Operations;
+import com.api.ReusableUtils.UserExcelReader;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -16,90 +19,175 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class UserLogin_Actions {
-	
-Reusable_CRUD_Operations restUtil= new Reusable_CRUD_Operations();
-private String requestBody = "";
-String token;
+
+	Reusable_CRUD_Operations restUtil = new Reusable_CRUD_Operations();
+	private String requestBody = "";
+	String token;
+	Response response;
+	int statusCode;
+	String diet_Email;
+	String diet_Pwd;
+	String patient_Email;
+	String patient_Pwd;
 	
 
-
-/*=================================reading credentials from properties file ======================================*/
-
-	public void readProperties()
-	{
-	Properties prop= new Properties();
-	InputStream input=null;
-	
-	
-	try {
-		//input= new FileInputStream("config.properties");
-		  input = getClass().getClassLoader().getResourceAsStream("Configs/config.properties");
-		  if (input == null) {
-              System.out.println("Sorry, unable to find Configs/config.properties");
-              return;
-		  }
-		prop.load(input);
-		String password= prop.getProperty("password");
-		String userLoginEmail= prop.getProperty("userLoginEmail");
-		
-		//System.out.println("password is"+ password);
-		//System.out.println("userLoginEmail is"+ userLoginEmail);
-		
-		 // Construct JSON payload using Gson
-        JsonObject json = new JsonObject();
-        json.addProperty("password", password);
-        json.addProperty("userLoginEmail", userLoginEmail);
-
-        Gson gson = new Gson();
-        requestBody = gson.toJson(json);
-		
-		
+	/*
+	 * ================================create json payload======================================
+	 */
+	public static String createJsonPayload(String... keyValuePairs) {
+		JsonObject json = new JsonObject();
+		for (int i = 0; i < keyValuePairs.length; i += 2) {
+			json.addProperty(keyValuePairs[i], keyValuePairs[i + 1]);
+		}
+		Gson gson = new Gson();
+		return gson.toJson(json);
 	}
-	catch(IOException ex)
-	{
-		ex.printStackTrace();
+
+	/*
+	 * =================================Building request
+	 * specification======================================
+	 */
+
+	public RequestSpecification buildRequest() throws FileNotFoundException {
+		RequestSpecification reqSpec;
+		reqSpec = restUtil.getRequestSpec();
+		return reqSpec;
 	}
-	finally
-	{
-		 if (input != null) {
-             try {
-                 input.close();
-             } catch (IOException e) {
-                 e.printStackTrace();
-             }
-         }
+	
+	/*
+	 * ============================UserLogin request to create auth token from
+	 * excel===============================================
+	 */
+
+	public Response loginToGetAuthorized_User(RequestSpecification reqSpec, String currentTag) throws InvalidFormatException, IOException
+			 {
+		String trimmedCurrentTag = currentTag.startsWith("@") ? currentTag.substring(1) : currentTag;
+		
+		List<Map<String, String>> getUserData = (UserExcelReader.getData(EnvConstants.Excelpath, "Dietician_data"));
+		Map<String, String> rowdata = getUserData.stream().filter(row -> row.get("scenario").equals(trimmedCurrentTag))
+				.findFirst()
+				.orElseThrow(() -> new RuntimeException("No matching data found for tag: " + trimmedCurrentTag));
+		String password = rowdata.get("password");
+		String userLoginEmail = rowdata.get("userLoginEmail");
+		String scenario = rowdata.get("scenario");
+		System.out.println("scenario from excel :" + scenario);
+		
+		requestBody = createJsonPayload("password", password, "userLoginEmail", userLoginEmail);
+		System.out.println("request body :" + requestBody);
+
+		// sending request
+		switch (trimmedCurrentTag) {
+		case "LoginPositive1":
+			response = restUtil.create(reqSpec, requestBody, EnvConstants.login_Endpoint);
+			System.out.println("Admin loggedin successfully");
+			break;
+
+		case "LoginInvalidCredential2":
+			response = restUtil.create(reqSpec, requestBody, EnvConstants.login_Endpoint);
+			break;
+		case "LoginInvalidMethod3":
+			response = restUtil.get(reqSpec,EnvConstants.login_Endpoint);
+
+		case "LoginInvalidEndpoint4":
+			response = restUtil.create(reqSpec, requestBody, EnvConstants.Invalidlogin_Endpoint);
+			break;
+		case "LoginInvalidContentType5":
+			RequestSpecification defaultSpec = restUtil.getRequestSpec();
+			RequestSpecification textspec = defaultSpec.contentType(io.restassured.http.ContentType.TEXT);
+			response = restUtil.create(textspec, requestBody, EnvConstants.login_Endpoint);
+			break;
+		default:
+			throw new RuntimeException("no matching tag :" + trimmedCurrentTag);
+		}
+
+		if (response.getStatusCode() == 200) {
+			String token = restUtil.extractStringFromResponse(response, "token");
+			System.out.println("The token from the response is " + token);
+			EnvVariables.token = token;
+			System.out.println("The token stored in EnvVariables.token is " + EnvVariables.token);
+
+		} else {
+			System.out.println("Valid Login Failed with status code: " + response.getStatusCode());
+		}
+
+		return response;
+
 	}
-}
+
+	/*
+	 * ===========================Dietcian Login
+	 * ======================================================================
+	 */
+
+	public Response DietLogin(RequestSpecification reqSpec,String currentTag) throws InvalidFormatException, IOException {
 	
-/*=================================Building request specification======================================*/
-	
-public RequestSpecification buildRequest() throws FileNotFoundException
-{
-	RequestSpecification reqSpec;
-	reqSpec = restUtil.getRequestSpec();
-	return reqSpec;
-}
- 	
+		if (currentTag.equals("@DieticianLogin1")) {
+		
+		diet_Email = EnvVariables.dietician1_Email;
+		diet_Pwd = EnvVariables.dietician1_loginPassword;
+		System.out.println("Dietician loggedin successfully");
 
-/*============================post request to create auth token===============================================*/
+		}
+		else if(currentTag.equals("@DieticianInvalid2")) {
+			
+			diet_Email = EnvConstants.Invalid_dietician_Email;
+			diet_Pwd = EnvConstants.Invalid_Dietician_pwd;
+		}
+		
+		// Construct JSON payload using Gson
 
-public Response loginToGetAuthorized_User(RequestSpecification reqSpec) throws FileNotFoundException {
-	readProperties();
-	System.out.println("Login request Body is : "+requestBody);
-	Response response = restUtil.create(reqSpec,requestBody, EnvConstants.login_Endpoint);
-	
-	return response;
-}
+		requestBody = createJsonPayload("password", diet_Pwd, "userLoginEmail", diet_Email);
+		System.out.println("request body : "+requestBody);
+		response = restUtil.create(reqSpec, requestBody, EnvConstants.login_Endpoint);
 
+		if (response.getStatusCode() == 200) {
+			String token = restUtil.extractStringFromResponse(response, "token");
+			System.out.println("The Dietician token from the response is " + token);
+			EnvVariables.Diet_token = token;
+			System.out.println("The Dietician token stored in EnvVariables.token is " + EnvVariables.Diet_token);
 
-/*===========================Storing the auth token in Env variables==============================================*/
+		} else {
+			System.out.println("Valid Login Failed with status code: " + response.getStatusCode());
+		}
+		return response;
+	}
 
-public void storeAuthToken(Response response)
-{
-	//System.out.println("response sending from actions "+response.asPrettyString());
-	String token= restUtil.extractStringFromResponse(response, "token");
-	System.out.println("The token from the response is "+ token);
-	EnvVariables.token=token;
-	System.out.println("The token stored in EnvVariables.token is "+ EnvVariables.token);
-}
+	/*
+	 * =====================================Patient
+	 * Login========================================================
+	 */
+	public Response PatientLogin(RequestSpecification reqSpec, String currentTag)
+			throws InvalidFormatException, IOException {
+		
+		String trimmedCurrentTag = currentTag.startsWith("@") ? currentTag.substring(1) : currentTag;
+		List<Map<String, String>> getUserData = (UserExcelReader.getData(EnvConstants.Excelpath, "Dietician_data"));
+		Map<String, String> rowdata = getUserData.stream().filter(row -> row.get("scenario").equals(trimmedCurrentTag))
+				.findFirst()
+				.orElseThrow(() -> new RuntimeException("No matching data found for tag: " + trimmedCurrentTag));
+		if (trimmedCurrentTag.equals("PatientLogin1")){
+		patient_Email = EnvVariables.patient1_Email1;
+		patient_Pwd = rowdata.get("password");
+		System.out.println("Patient loggedin successfully");
+		}
+		else if(trimmedCurrentTag.equals("PatientInvalidLogin2")) {
+			patient_Email = rowdata.get("userLoginEmail");
+			patient_Pwd = rowdata.get("password");
+			}
+		// Construct JSON payload using Gson
+
+		requestBody = createJsonPayload("password", patient_Pwd, "userLoginEmail", patient_Email);
+		response = restUtil.create(reqSpec, requestBody, EnvConstants.login_Endpoint);
+		statusCode = response.getStatusCode();
+
+		if (response.getStatusCode() == 200) {
+			String token = restUtil.extractStringFromResponse(response, "token");
+			System.out.println("The Patient token from the response is " + token);
+			EnvVariables.Patient_token = token;
+			System.out.println("The Patient token stored in EnvVariables.token is " + EnvVariables.Patient_token);
+
+		} else {
+			System.out.println("Valid Login Failed with status code: " + response.getStatusCode());
+		}
+		return response;
+	}
 }
